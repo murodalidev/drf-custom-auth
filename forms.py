1acda7423b184e6faf4d95c656c768ccd8fc89c2
Murodali Narzullaev <61009662+murodalidev@users.noreply.github.com>
2022-10-27 19:55:06 +0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,23 +2,25 @@ from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-from .models import User
+from .models import Account
 
 
-class UserCreationForm(forms.ModelForm):
+class AccountCreationForm(forms.ModelForm):
     password1 = forms.CharField(label=_('Password'), widget=forms.PasswordInput)
     password2 = forms.CharField(label=_('Password confirmation'), widget=forms.PasswordInput)
 
     class Meta:
-        model = User
+        model = Account
         fields = ('username', )
 
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError(_('Password don\'t match'))
-        return password2
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError(_('Password don\'t match'))
+            return password2
+        raise forms.ValidationError(_('You should write passwords'))
 
     def save(self, commit=True):
         account = super().save(commit=False)
@@ -28,17 +30,18 @@ class UserCreationForm(forms.ModelForm):
         return account
 
 
-class UserChangeForm(forms.ModelForm):
+class AccountChangeForm(forms.ModelForm):
     password = ReadOnlyPasswordHashField()
 
     class Meta:
-        model = User
+        model = Account
         fields = ('username', 'email', 'full_name', 'phone', 'image', 'is_superuser', 'is_staff', 'is_active')
 
     def __init__(self, *args, **kwargs):
-        super(UserChangeForm, self).__init__(*args, **kwargs)
+        super(AccountChangeForm, self).__init__(*args, **kwargs)
         self.fields['password'].help_text = '<a href="%s">change password</a>.' % reverse_lazy(
             'admin:auth_user_password_change', args=[self.instance.id])
 
     def clean_password(self):
         return self.initial['password']
+
