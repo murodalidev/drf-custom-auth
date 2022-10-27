80fa176aace973dec0816e74ac2328d917cd1d3b
Murodali Narzullaev <61009662+murodalidev@users.noreply.github.com>
2022-10-27 19:55:25 +0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,28 +1,28 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
-from .forms import UserCreationForm, UserChangeForm
+from .models import Account
+from .forms import AccountCreationForm, AccountChangeForm
 
 
-class UserAdmin(BaseUserAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
+class AccountAdmin(BaseUserAdmin):
+    form = AccountChangeForm
+    add_form = AccountCreationForm
     list_display = ('id', 'username', 'email', 'full_name', 'phone', 'image_tag', 'is_superuser', 'is_staff',
-                    'is_active', 'is_verified', 'date_login', 'date_created')
-    readonly_fields = ('date_login', 'date_created')
-    list_filter = ('date_created', 'is_superuser', 'is_staff', 'is_active', 'is_verified')
+                    'is_active', 'date_modified', 'date_created')
+    readonly_fields = ('date_modified', 'date_created')
+    list_filter = ('date_created', 'is_superuser', 'is_staff', 'is_active')
     ordering = ()
     fieldsets = (
         (None, {'fields': ('username', 'email', 'full_name', 'phone', 'image', 'password')}),
-        (_('Permissions'), {'fields': ('is_superuser', 'is_staff', 'is_active', 'is_verified',
+        (_('Permissions'), {'fields': ('is_superuser', 'is_staff', 'is_active',
                                        'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('date_login', 'date_created')}),
+        (_('Important dates'), {'fields': ('date_modified', 'date_created')}),
     )
     add_fieldsets = (
-        (None, {'classes': ('wide',), 'fields': ('email', 'password1', 'password2'), }),
+        (None, {'classes': ('wide',), 'fields': ('username', 'password1', 'password2'), }),
     )
     search_fields = ('username', 'email', 'full_name', 'phone')
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(Account, AccountAdmin)
