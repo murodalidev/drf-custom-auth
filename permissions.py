b33c1a4e3740cf32f4cda72aa9a7ad84a91a9111
Murodali Narzullaev <61009662+murodalidev@users.noreply.github.com>
2022-10-27 17:52:20 +0500

    Create permissions.py

---
+++ permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsOwnerForUser(BasePermission):
+    message = 'Access to only owner'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.id == request.user.
