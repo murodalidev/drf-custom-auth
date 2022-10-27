c54f841db197d94b0b03656728d53f43055df3c8
Murodali Narzullaev <61009662+murodalidev@users.noreply.github.com>
2022-10-27 18:06:30 +0500

    Update permissions.py

--- permissions.py
+++ permissions.py
@@ -7,4 +7,4 @@ class IsOwnerForUser(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        return obj.id == request.user.
+        return obj.id == request.user.id
